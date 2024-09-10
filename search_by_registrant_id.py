361fa5a5c8db1350804df0e1ee65ce8ff8aab923
Heather <heathermcaslan@macbook-pro-4.home>
2024-09-10 17:43:29 +0100

    streamlined search by id

--- search_by_registrant_id.py
+++ search_by_registrant_id.py
@@ -3,43 +3,43 @@ import json
 import requests
 import sys
 
-import util.map
-import util.config_parser
+from util import map, config_parser
 from api import regfox
 from participants.artist_guest import Artist_Guest
 from participants.student import Student
 from participants.chaperone import Chaperone
-
-
-class FormIds:
-	ARTIST_GUEST = util.config_parser.read_config()['artist_guest_form']
-	STUDENT = util.config_parser.read_config()['student_form']
-	CHAPERONE = util.config_parser.read_config()['chaperone_form']
+from participants.crew import Crew
+from participants.sound_engineer_guest import Sound_Engineer_Guest
+from participants.board_member import Board_Member
 
 def main():
 	if len(sys.argv) != 2:
 		print("Error! Usage: python3 search_by_registrant_id.py '<id>'")
 		exit()
+	
 	else:
 		id = sys.argv[1]
-		response = json.loads(regfox.get_registrants_by_id(id))	
-		data = response['data']
-		form_id = data['formId']
-		print('\n', data, '\n')
 		
-		role_dict = util.map.all_fields(data)
-		print('\n', role_dict, '\n')
+		response = json.loads(regfox.get_registrants_by_id(id))	
+		response_data = response['data']
+		form_id = str(response_data['formId'])
+		print('\n', response_data, '\n')
 
-		match form_id:
-			case FormIds.ARTIST_GUEST:
-				artist_guest = Artist_Guest(role_dict)
-				print(artist_guest.to_row(), '\n')
-			case FormIds.STUDENT:
-				student = Student(role_dict)
-				print(student.to_row(), '\n')
-			case FormIds.CHAPERONE:
-				chaperone = Chaperone(role_dict)
-				print(chaperone.to_row(), '\n')
+		# try to map the registrant to a known form role
+		config_data = config_parser.read_config()	
+		forms = {}
+		forms[config_data['artist_guest_form']] = Artist_Guest
+		forms[config_data['crew_form']] = Crew
+		forms[config_data['sound_engineer_guest_form']] = Sound_Engineer_Guest
+		forms[config_data['student_form']] = Student
+		forms[config_data['chaperone_form']] = Chaperone
+		forms[config_data['board_form']] = Board_Member
+
+		participant_dict = map.all_fields(response_data)
+
+		participant = forms[form_id](participant_dict)
+
+		print(participant.to_row(), '\n')
 
 if __name__ == '__main__':
 	main()
